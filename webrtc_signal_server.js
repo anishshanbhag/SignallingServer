f7a94f0cba619a03e27e9f7551af184cc48b6c1c
Anish Shanbhag <anishshanbhag15@gmail.com>
2018-06-26 13:45:37 +0530

    added envport and procfile

--- webrtc_signal_server.js
+++ webrtc_signal_server.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var websocket = require("websocket").server;
 
 // general variables
-var port = 1234;
+var port = process.env.PORT;
 var webrtc_clients = [];
 var webrtc_discussions = {};
 
